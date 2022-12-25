6ed4943083d50315a4d7104f2f521a97bfdd1253
Aryan Sangwan <55026638+ATV3320@users.noreply.github.com>
2022-12-25 13:29:49 +0530

    a little more

--- new_folder/src/main.rs
+++ new_folder/src/main.rs
@@ -10,4 +10,31 @@ fn main() {
 
     let q = "parh";
     println!("Changed value of q is {}", q);
+
+    // a good lesson here,:
+    {
+        let q = 4;
+        println!("In the scope q is: {}", q);
+    }
+    // q = q +1;
+    // throws error since outside of the scope, q is still a string.
+    println!("I am {}", q);
+
+    let mut r = 10;
+    let mut s = 10;
+    println!("check 1, value of r is {}", r);
+    println!("check 1, value of s is {}", s);
+
+    {
+        r = r-5;
+        let s = s-5;
+        println!("check 2, value of r is {}", r);
+        println!("check 2, value of s is {}", s);
+    }
+    r = r + 5;
+    s = s + 5;
+    println!("check 3, value of r is {}", r);
+    println!("check 3, value of s is {}", s);
+    //so we can say that in this case, only one reference of r is accessed at both times,
+    //but s was accessed differently.
 }
