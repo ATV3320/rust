a81b99e06e6848a612f09e1ffe461c34cd29c5e8
Aryan Sangwan <55026638+ATV3320@users.noreply.github.com>
2022-12-26 16:01:12 +0530

    tried using two files

---
+++ new_folder/src/trialFile1.rs
@@ -0,0 +1,3 @@
+pub fn main() {
+    println!("I am getting printed from a different file, so you have accessed me from your main.rs");
+}
---
+++ new_folder/target/debug/deps/new_folder-bb68aeea7b7a1848.d
@@ -0,0 +1,6 @@
+/home/user/Documents/GitHub/rust/new_folder/target/debug/deps/new_folder-bb68aeea7b7a1848.rmeta: src/main.rs src/trialFile1.rs
+
+/home/user/Documents/GitHub/rust/new_folder/target/debug/deps/new_folder-bb68aeea7b7a1848.d: src/main.rs src/trialFile1.rs
+
+src/main.rs:
+src/trialFile1.rs:
