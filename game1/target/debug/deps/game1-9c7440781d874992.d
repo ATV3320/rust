44527f2c93e195db92c7033df3570e259b2ea714
Aryan Sangwan <55026638+ATV3320@users.noreply.github.com>
2022-12-29 23:24:28 +0530

    bro again?

---
+++ game1/target/debug/deps/game1-9c7440781d874992.d
@@ -0,0 +1,5 @@
+/home/atv/Documents/GitHub/rust/game1/target/debug/deps/game1-9c7440781d874992.rmeta: src/main.rs
+
+/home/atv/Documents/GitHub/rust/game1/target/debug/deps/game1-9c7440781d874992.d: src/main.rs
+
+src/main.rs:
